240c480cde50a56c3d45ca76f43205ca25a9f081
Gerard Pelly <gerardwp@gmail.com>
2019-07-21 20:22:37 +1000

    temporary click function - i believe i will need to make a new one for when the buttons are added via the form input - api is working

---
+++ assets/javascript/gify.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+
+    var topics = ["dog", "fox", "bear", "cat", "sloth"];
+
+
+    // var apiKey = "LU5QsIl3S1DSEVAfaJpYZcM6rrQs91Y1";
+
+    // var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=" + apiKey + "&limit=10";
+
+    function createButtons() {
+
+        $(".button-zone").empty();
+
+        for (var i = 0; i < topics.length; i++) {
+
+            var pageBtn = $("<button>");
+
+            pageBtn.addClass("topic");
+            pageBtn.attr("data-name", topics[i]);
+            pageBtn.text(topics[i]);
+            $(".button-zone").append(pageBtn);
+        };
+
+    };
+
+    createButtons();
+
+
+    // temporary click function for pre-generated buttons
+    $(".topic").on("click", function () {
+
+        var apiKey = "LU5QsIl3S1DSEVAfaJpYZcM6rrQs91Y1";
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=" + apiKey + "&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        });
+
+
+    });
+
+
+
+
+
+
+
+});
